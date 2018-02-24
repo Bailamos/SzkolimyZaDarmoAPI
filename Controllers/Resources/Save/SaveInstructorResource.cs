8e957f664096e5a752adbda34fe7d87ccd56cc77
Bailamos <bailamos@github.com>
2018-02-24 15:37:58 +0100

    Insturctor fields validation

--- Controllers/Resources/Save/SaveInstructorResource.cs
+++ Controllers/Resources/Save/SaveInstructorResource.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Szkolimy_za_darmo_api.Controllers.Resources.Save
 {
     public class SaveInstructorResource
     {
+        [RegularExpression("^(\\d{7}|\\d{9})$")]
         public string PhoneNumber {get; set;}
         public string Name {get; set;}
         public string Surname {get; set;}
+
+        [RegularExpression(
+            "^((([!#$%&'*+\\-/=?^_`{|}~\\w])|([!#$%&'*+\\-/=?^_`{|}~\\w]"
+             + "[!#$%&'*+\\-/=?^_`{|}~\\.\\w]{0,}[!#$%&'*+\\-/=?^_`{|}~\\w]))"
+             + "[@]\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*)$")]
         public string Email {get; set;}
     }
 }
